237b93fa1a595c3a8dabb7d3efbdb1f5a49f9203
hnaasami017 <hnaasami017@gmail.com>
2022-10-25 20:32:28 +0200

    Adding new feature ny Hanaa

---
+++ Hanaa_Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hanaa_Main {}
+
+    public static void main(String[] args) {
+        List<String> WEBControlTools = new ArrayList<>();
+        WEBControlTools.addAll(List.of("git", "SVN", "Bazar"));
+
+        for (String Tool : WEBControlTools) {
+            System.out.println(Tool);
+
+        }
+    }
+
