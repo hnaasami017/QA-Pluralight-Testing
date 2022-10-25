1af77b96197ebf1afa1bb2fa258729e6a910fd4c
hnaasami017 <hnaasami017@gmail.com>
2022-10-25 20:17:15 +0200

    Adding new feature

--- main.java
+++ main.java
@@ -1,5 +1,17 @@
-public class main
-{
+import java.util.ArrayList;
+import java.util.List;
 
+public class main {
+    public static void main(String[] args)
+    {
+        List<String> SourceControlTools = new ArrayList<>() ;
+        SourceControlTools.addAll(List.of("git","SVN", "Bazar")) ;
+
+        for (String Tool:SourceControlTools) {
+            System.out.println(Tool);
+
+        }
+
+    }
 
 }
